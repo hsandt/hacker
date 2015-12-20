8be1e1a15c9f749ae659e2e6d009c0731cf2d90a
hsandt <hs@fuwar.in>
2015-12-20 01:32:56 +0100

    [HUB] Fixed parallax (WIP)

--- src/js/hub.js
+++ src/js/hub.js
@@ -5,17 +5,24 @@
 
     this.initHub = function() {
 
-        console.log("HUB ready function");
+        console.log("Init HUB");
 
+        // bind "open modular window" event to each monitor
         var dlgtrigger = document.querySelectorAll('[data-dialog]');
         for (var i = 0; i < dlgtrigger.length; i++) {
-            console.log(i);
+            //console.log(i);
             somedialog = document.getElementById(dlgtrigger[i].getAttribute('data-dialog'));
             dlg = new DialogFx(somedialog);
 
 
             dlgtrigger[i].addEventListener('click', dlg.toggle.bind(dlg));
         }
+
+        // activate parallax from mouse
+        $('#parallax .parallax-layer')
+            .parallax({
+                mouseport: $('#parallax')
+            });
 
     };
 
